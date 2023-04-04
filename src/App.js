270ec60a9f9987f4dbdf260c85663b11e6dbed2c
Lê Hoàng Long <77973385+lehoanglong12345@users.noreply.github.com>
2023-04-04 12:21:19 +0700

    Setup structure directory of projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import './sass/style.css';
+import Button from './components/Button/Button';
+import {  } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Router>
+        <div className='header'>
+          <div className='header-inner'>
+            <div className='header-inner-text'>
+              HELLO GUYS
+            </div>
+            <p style={{ fontSize: 40 }}>Hello</p>
+          </div>
+          <div className='box'></div>
+        </div>
+        <div style={{ padding: '10px 32px' }}>
+          <Button />
+          <Button primary />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
+  )
 }
 
 export default App;
