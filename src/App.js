576e58ee410fb58b8d2d438d364fcc7c0ee67f66
lehoanglong12345 <lelong.ll32@gmail.com>
2023-04-13 14:29:01 +0700

    Create Forgot Password Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/forgot_password' element={<ForgotPassword />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
