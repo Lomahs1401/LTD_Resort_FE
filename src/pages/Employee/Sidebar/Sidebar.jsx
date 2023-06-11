bb6aed979b47fbd9169389fb230dee790d01c506
Lê Hoàng Long <lelong.ll32@gmail.com>
2023-06-11 14:10:39 +0700

    Update Sidebar.jsx
    AA

--- src/pages/Employee/Sidebar/Sidebar.jsx
+++ src/pages/Employee/Sidebar/Sidebar.jsx
@@ -118,7 +118,7 @@ const Sidebar = () => {
                 ml="15px"
               >
                 <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINIS
+                  EMPLOYEE
                 </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
